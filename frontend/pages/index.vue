9c3ffe1d34017b8019c8244b717462893fcc9ec5
shemzz <davidshemang@gmail.com>
2020-11-30 15:15:38 +0100

    set frontpage

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,29 @@
 <template>
- <div class="columns">
-   <div class="column is-danger">
-     <nuxt-link to="/login">login</nuxt-link>
-   </div>
-<div class="column is-info">
-     <nuxt-link to="/register">Register</nuxt-link>
-   </div>
-   <div class="column is-primary">
-     <nuxt-link to="/dashboard">Dashboard</nuxt-link>
-   </div>
+<div>
+<div class="tabs is-centered">
+  <ul>
+    <li class="is-active"><nuxt-link to="/">Home</nuxt-link></li>
+    <li><nuxt-link to="/login">login</nuxt-link></li>
+    <li><nuxt-link to="/register">Register</nuxt-link></li>
+    <li><nuxt-link to="/dashboard">Dashboard</nuxt-link></li>
+  </ul>
+</div>
+<div class="tile">
+   <div class="tile is-parent">
+      <article class="tile is-child notification is-danger">
+        <p class="title">Hello</p>
+        <p class="subtitle">This is a rush work (obviously) and I hope I am in line with your requirement.</p>
+        <div class="content">
+          <!-- Content -->
+          This small site was built with NuxtJs on the frontend and AdonisJS for the backend. however, due to time constraint, I was able to implement very little of the backend code into the frontend. <br> The links to the pages requested are listed up above. <br>
+          The link to Github Repo is <a href="https://github.com/shemzz/PetBoX/tree/frontend/frontend">HERE</a> <br>
+          If this looks like what you intend to build and you hire me, I can build out your specific requirement.
+
+<br><br> Regards, <br> Shemang David J. <br> <a href="tel:+2348035542225">08035542225</a> <br><a href="mailto:davidshemang@gmail.com">davidshemang@gmail.com</a> <br> <a href="https://twitter.com/shemang_david">My Twitter Profile</a>
+        </div>
+      </article>
     </div>
+</div>
+</div>
 </template>
 
